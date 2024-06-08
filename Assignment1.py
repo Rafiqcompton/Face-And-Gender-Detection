5b676ad65c1e9a5c6ef131d70bddb91b8b7c438e
Hihi_root <124554329+Rafiqcompton@users.noreply.github.com>
2024-06-08 12:52:39 -0700

    Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,64 +1,82 @@
-
-'''
-Assigment 1 Detect Faces from static Images Predict their Gender and the Age of the Person/s in the Photo 
-'''
-
-
 import cv2
+import math
+import sys
 
+# Defined the model files
+FACE_PROTO = "weights/opencv_face_detector.pbtxt"
+FACE_MODEL = "weights/opencv_face_detector_uint8.pb"
 
+AGE_PROTO = "weights/age_deploy.prototxt"
+AGE_MODEL = "weights/age_net.caffemodel"
 
-face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+GENDER_PROTO = "weights/gender_deploy.prototxt"
+GENDER_MODEL = "weights/gender_net.caffemodel"
 
-
-img = cv2.imread('./Images/img1.jpg', 1)  # Load in color mode (1)
-
-
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-
-GENDER_MODEL = 'Weights/deploy_gender.prototxt'  # Path to gender model text file
-GENDER_PROTO = 'Weights/gender_net.caffemodel'  # Path to gender model weights
-AGE_MODEL = 'weights/deploy_age.prototxt'  # Path to age model text file
-AGE_PROTO = 'weights/age_net.caffemodel'  # Path to age model weights
-
+# Load network
+FACE_NET = cv2.dnn.readNet(FACE_MODEL, FACE_PROTO)
+AGE_NET = cv2.dnn.readNet(AGE_MODEL, AGE_PROTO)
+GENDER_NET = cv2.dnn.readNet(GENDER_MODEL, GENDER_PROTO)
 
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
-GENDER_LIST = ['Male', 'Female']
-AGE_INTERVALS = ['(0, 2)', '(4, 6)', '(8, 12)', '(15, 20)',
-                 '(25, 32)', '(38, 43)', '(48, 53)', '(60, 100)']
+AGE_LIST = ["(0-2)", "(4-6)", "(8-12)", "(15-20)", "(25-32)", "(38-43)", "(48-53)", "(60-100)"]
+GENDER_LIST = ["Male", "Female"]
 
+box_padding = 20
 
-faces = face_classifier.detectMultiScale(gray_image, 1.3, 2)
+def get_face_box (net, frame, conf_threshold = 0.7):
+  frame_copy = frame.copy()
+  frame_height = frame_copy.shape[0]
+  frame_width = frame_copy.shape[1]
+  blob = cv2.dnn.blobFromImage(frame_copy, 1.0, (300, 300), [104, 117, 123], True, False)
 
+  net.setInput(blob)
+  detections = net.forward()
+  boxes = []
 
-for (x, y, w, h) in faces:
+  for i in range(detections.shape[2]):
+    confidence = detections[0, 0, i, 2]
 
-    # Draw a rectangle around the face
-    cv2.rectangle(img, (x, y), (x + w, y + h), (100, 25, 70), 5, cv2.LINE_AA)
+    if confidence > conf_threshold:
+      x1 = int(detections[0, 0, i, 3] * frame_width)
+      y1 = int(detections[0, 0, i, 4] * frame_height)
+      x2 = int(detections[0, 0, i, 5] * frame_width)
+      y2 = int(detections[0, 0, i, 6] * frame_height)
+      boxes.append([x1, y1, x2, y2])
+      cv2.rectangle(frame_copy, (x1, y1), (x2, y2), (0, 255, 0), int(round(frame_height / 150)), 8)
 
-    # Gender and Age Prediction 
-    if GENDER_MODEL and GENDER_PROTO and AGE_MODEL and AGE_PROTO:
-        # Preprocess the face region for prediction
-        face_blob = cv2.dnn.blobFromImage(img[y:y + h, x:x + w], 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+  return frame_copy, boxes
 
-        # Load and make predictions using gender and age models 
-        face_net = cv2.dnn.readNetFromCaffe(GENDER_MODEL, GENDER_PROTO)
-        age_net = cv2.dnn.readNetFromCaffe(AGE_MODEL, AGE_PROTO)
+def age_gender_detector (input_path):
+  image = cv2.imread(input_path)
+  resized_image = cv2.resize(image, (640, 480))
 
-        face_net.setInput(face_blob)
-        gender_preds = face_net.forward()
-        gender = GENDER_LIST[gender_preds[0].argmax()]
+  frame = resized_image.copy()
+  frame_face, boxes = get_face_box(FACE_NET, frame)
 
-        age_net.setInput(face_blob)
-        age_preds = age_net.forward()
-        age = AGE_INTERVALS[age_preds[0].argmax()]
+  for box in boxes:
+    face = frame[max(0, box[1] - box_padding):min(box[3] + box_padding, frame.shape[0] - 1), \
+      max(0, box[0] - box_padding):min(box[2] + box_padding, frame.shape[1] - 1)]
 
-        # Display predicted gender and age above the face
-        cv2.putText(img, gender, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (36, 255, 12), 2, cv2.LINE_AA)
-        cv2.putText(img, age, (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (36, 255, 12), 2, cv2.LINE_AA)
+    blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB = False)
+    GENDER_NET.setInput(blob)
+    gender_predictions = GENDER_NET.forward()
+    gender = GENDER_LIST[gender_predictions[0].argmax()]
+    print("Gender: {}, conf: {:.3f}".format(gender, gender_predictions[0].max()))
 
-# Display the final image with detected faces (and predicted gender/age if applicable)
-cv2.imshow("Detection", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    AGE_NET.setInput(blob)
+    age_predictions = AGE_NET.forward()
+    age = AGE_LIST[age_predictions[0].argmax()]
+    print("Age: {}, conf: {:.3f}".format(age, age_predictions[0].max()))
+
+    label = "{},{}".format(gender, age)
+    cv2.putText(frame_face, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
+
+  return frame_face
+
+if __name__ == "__main__":
+  output = age_gender_detector(sys.argv[1])
+  cv2.imwrite("output/output.jpg", output)
+  cv2.imshow("Output", output)
+
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
